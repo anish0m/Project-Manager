f3cfb5846e6dfe317c3b3776a40a27c7cc3a8efb
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2026-05-11 11:53:12 +0600

    Implement task deletion functionality and enhance NewTask component with input validation

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,14 @@ function App() {
     });
   }
 
-  function handleDeleteTask() {}
+  function handleDeleteTask(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  }
 
   const selectedProject = projectsState.projects.find(
     (project) => project.id === projectsState.selectedProjectId,
@@ -120,6 +127,7 @@ function App() {
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
         onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
--- src/components/views/NewTask.jsx
+++ src/components/views/NewTask.jsx
@@ -1,31 +1,51 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+
+import Modal from "../ui-elements/Modal";
 
 export default function NewTask({ onAdd }) {
-  const [enteredTask, setEnteredTask] = useState();
+  const modal = useRef();
+
+  const [enteredTask, setEnteredTask] = useState("");
 
   function handleChange(event) {
     setEnteredTask(event.target.value);
   }
 
   function handleClick() {
+    if (enteredTask.trim() === "") {
+      modal.current.open();
+      return;
+    }
+
     onAdd(enteredTask);
     setEnteredTask("");
   }
 
   return (
-    <div>
-      <input
-        type="text"
-        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
-        onChange={handleChange}
-        value={enteredTask}
-      />
-      <button
-        className="mx-2 text-stone-700 hover:text-stone-950"
-        onClick={handleClick}
-      >
-        Add Task
-      </button>
-    </div>
+    <>
+      <Modal ref={modal} buttonText="Okay">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          <span className="block">Cannot add an empty task.</span>
+          <span className="block">
+            Please enter a valid task description and try again.
+          </span>
+        </p>
+      </Modal>
+      <div>
+        <input
+          type="text"
+          className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+          onChange={handleChange}
+          value={enteredTask}
+        />
+        <button
+          className="mx-2 text-stone-700 hover:text-stone-950"
+          onClick={handleClick}
+        >
+          Add Task
+        </button>
+      </div>
+    </>
   );
 }
